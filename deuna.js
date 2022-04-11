7f9b0945123174352966281d9d603754718a7d54
Rail Neira <neira.rail@gmail.com>
2022-04-10 23:35:09 -0400

    Lee y valida[sin implementar] clase

--- deuna.js
+++ deuna.js
@@ -1,7 +1,20 @@
 const fs = require('fs')
-const java_class_path = process.argv[2]
 
+const java_class_path = process.argv[2]
+let java_class_text = ''
+//Leer archivo y guardarlo en una variable
 fs.readFile(java_class_path, 'utf-8', (error, datos)=>{
     if(error) throw error;
-    console.log(datos)
+    java_class_text = datos
 })
+
+//Validar una clase de java + Springboot
+function validar_java_class(text){
+    return true
+}
+
+if(!validar_java_class(java_class_text)){
+    throw Error("No se validó la clase")
+}
+
+console.log("Holaaa")
