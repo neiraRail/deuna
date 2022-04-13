9da4cefcafd09e07850d6638bfa77989f5f20314
Rail Neira <neira.rail@gmail.com>
2022-04-12 21:29:31 -0400

    Se extrae funcionalidad de validador a clase externa

--- deuna.js
+++ deuna.js
@@ -1,12 +1,15 @@
 const fs = require('fs')
 var Extractor = require('./extractor.js')
+var Validador = require('./validador.js')
 const extractor = new Extractor()
+const validador = new Validador()
+
 const java_class_path = process.argv[2]
 
 //Leer archivo y guardarlo en una variable
 const java_class_text = fs.readFileSync(java_class_path, {encoding: 'utf8'})
 
-if(!validar_java_class(java_class_text)){
+if(!validador.validar_java_class(java_class_text)){
     throw Error("No se validó la clase")
 }
 
@@ -24,10 +27,7 @@ const model= {
 }
 
 console.log(model)
-//Validar una clase de java + Springboot
-function validar_java_class(text){
-    return text.length>0
-}
+
 
 // //Escribir en un archivo
 // fs.writeFile('exports/NombreClase.txt', content[0], (err) => {
---
+++ validador.js
@@ -0,0 +1,12 @@
+//Validar una clase de java + Springboot
+module.exports = class Validador {
+    constructor(){
+
+    }
+    validar_java_class(text){
+        return text.length>0
+    }
+}
+
+
+
