3a5f844c8f2e441fd20c132a34c962f030f30661
Rail Neira <neira.rail@gmail.com>
2022-04-16 13:46:53 -0400

    Se quita botón de reset

--- templates/tabla.vue
+++ templates/tabla.vue
@@ -95,14 +95,6 @@
         mdi-delete
       </v-icon>
     </template>
-    <template v-slot:no-data>
-      <v-btn
-        color="primary"
-        @click="initialize"
-      >
-        Reset
-      </v-btn>
-    </template>
   </v-data-table>
 </template>
 
